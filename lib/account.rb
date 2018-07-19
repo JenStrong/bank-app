7b144447778918eb2e950a67cb5eda34f69afb51
Jen Strong <jenniferastrong@gmail.com>
2018-07-19 10:54:31 +0100

    Change attr_accessor to attr_reader

--- lib/account.rb
+++ lib/account.rb
@@ -2,7 +2,7 @@ require_relative 'formatter'
 
 class Account
 
-  attr_accessor :formatter, :account_summary, :balance
+  attr_reader :formatter, :account_summary, :balance
 
   def initialize(formatter = Formatter.new)
     @formatter = formatter
@@ -11,12 +11,12 @@ class Account
   end
 
   def credit(transaction)
-    self.balance += transaction.amount
+    @balance += transaction.amount
     account_summary.push([transaction, balance])
   end
 
   def debit(transaction)
-    self.balance -= transaction.amount
+    @balance -= transaction.amount
     account_summary.push([transaction, balance])
   end
 
